cd3c699357e5e388188985eb9eeada72d1dc3cc5
Admin <Admin@DOTNET>
2019-12-08 18:15:51 +0530

    Added  wcfSayHello code to git

---
+++ wcfSayHello/Wishes.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace wcfSayHello
+{
+    public class Wishes
+    {
+        public string GetWishes(string name)
+        {
+            var src = DateTime.Now;
+            var hrs=src.Hour;
+            string greet = string.Empty;
+            string gretMessage = string.Empty;
+            if (hrs < 12)
+            {
+                greet = "Good Morning";
+            }
+            else if (hrs >= 12 && hrs <= 17)
+            {
+                greet = "Good Afternoon";
+            }
+            else if (hrs >= 17 && hrs <= 24)
+            {
+                greet = "Good Evening";
+            }
+            gretMessage = greet + "," + name;
+
+            return gretMessage;
+        }
+        public string GetGreetWishes(string name)
+        {
+            
+            string gretMessage = string.Empty;
+            gretMessage = "Happy weekend" + "," + name;
+
+            return gretMessage;
+        }
+    }
+}
